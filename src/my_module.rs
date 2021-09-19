0edf24377db22bce342baa1dd04c89de0d8b99f1
shaka <shmurakami1104@gmail.com>
2021-09-19 14:52:10 +0900

    inner function

--- src/my_module.rs
+++ src/my_module.rs
@@ -8,6 +8,18 @@ impl MyModule {
             n
         }
     }
+
+    fn factorial(&self, n: i32) -> i32 {
+        fn go(n: i32, acc: i32) -> i32 {
+            if n < 1 {
+                acc
+            } else {
+                go(n - 1, n * acc)
+            }
+        }
+        go(n, 1)
+    }
+
 }
 
 #[cfg(test)]
@@ -19,5 +31,14 @@ mod my_module_test {
         let my_mod = MyModule {};
         assert_eq!(10, my_mod.abs(10));
         assert_eq!(10, my_mod.abs(-10));
+    }
+
+    #[test]
+    fn factorial() {
+        let my_mod = MyModule {};
+        assert_eq!(1, my_mod.factorial(1));
+        assert_eq!(24, my_mod.factorial(4));
+        // panicked at attempt to multiply with overflow
+        // assert_eq!(1, my_mod.factorial(1000));
     }
 }
